b10bb8db8477e7b9ea2a15df1cbb211fff964def
Rafael PacMe <rafy.pachecomendez@gmail.com>
2017-03-26 10:53:20 -0300

    Peregrine: Welcome GlazeOS Signed by: @ROM-PacMe

--- glaze.mk
+++ glaze.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common GlazeOS stuff.
+$(call inherit-product, vendor/glaze/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
-PRODUCT_NAME := lineage_peregrine
+PRODUCT_NAME := glaze_peregrine
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
